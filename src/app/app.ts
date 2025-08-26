053952f63c54bd8581258a87126eaa3fee26f061
marco-soria <marco.soriaa7@gmail.com>
2025-08-26 15:40:53 -0500

    created footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from '../layout/footer/footer';
+import { Navbar } from '../layout/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'bookadventure-angular';
